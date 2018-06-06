61de64b3498d43d616a5f12cbaa35de473619718
Marina Evteeva <marina@detki-predki.ru>
2018-06-06 22:25:33 +0300

    Updated for the negative numbers case
    Last version didn't pass the tests so I assumed that it failed the negative part (even though there is no recommendation how to react). Maybe one more problem is with big numbers but for now i have no idea how to handle them without libraries

--- task2.js
+++ task2.js
@@ -1,22 +1,24 @@
 /* Task 2 */
 
-var num = process.argv[2];
-//bc there is no info about how I should react to negative numbers I am assuming it is 0
+var num = process.argv[2], flag = 0;
 if (num <= 0){
-    process.stdout.write("0");
-    return 0;
+    flag = 1;
+    num = -num;
 }
 
 //and I am not a big fan of recursion so
-var f1 = 1, f2 = 0, temp;
+var f1 = 1, f2 = 0, temp, i = num;
 
-while (num > 0){
+while (i > 0){
     temp = f1;
     f1 = f1 + f2;
     f2 = temp;
-    num--;
+    i--;
 }
-/*and I am assuming for now that the fact that the biggest number is 10000 is a mistake
+/*and I hope for now that the fact that the biggest number is 10000 is a mistake
 bc it is not possible with just a standart Number type and when libraries is not allowed
 I dont think I need this course if Im required to write my own BigNumbers library to be enrolled*/
+if (flag){
+    f2 = Math.pow(-1, num + 1)*f2;
+}
 process.stdout.write(f2.toString());
