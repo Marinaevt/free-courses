88d161a957cdb150bfab52b4a2be1d3a8ce2d4b2
Marina Evteeva <marina@detki-predki.ru>
2018-06-08 19:46:12 +0300

    Apparently Y is now a vowel, lol. Still not sure about the spaces, but hope i guessed it right

--- task3.js
+++ task3.js
@@ -2,18 +2,20 @@
 
 var args = process.argv.slice(2);
 var notwntsym = /[^A-Za-z0-9]/g;
-var vowels = /[aeiouAEIOU]/g;
+var vowels = /[aeiouyAEIOUY]/g;
 var num = /[0-9]/g;
 var str = args.join('').replace(notwntsym, '');
 var vow = str.match(vowels);
 var strnum = str.match(num);
 if (vow){
-    process.stdout.write(`${vow.join("")} `);
+    process.stdout.write(vow.join(""));
 }
+process.stdout.write(" ")
 var cons = str.replace(vowels, '').replace(num, '')
 if (cons){
-    process.stdout.write(`${cons} `);
+    process.stdout.write(cons);
 }
+process.stdout.write(" ")
 if (strnum){
     process.stdout.write(strnum.join(""));
 }
